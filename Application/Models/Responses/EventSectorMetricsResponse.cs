8984b0c5058b7862ba15fc473e790a31593088dc
alvaroramirezbrusco <alvaroramirezbrusco@gmail.com>
2025-11-23 15:54:49 -0300

    Metricas

---
+++ Application/Models/Responses/EventSectorMetricsResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.Models.Responses
+{
+    public class EventSectorMetricsResponse
+    {
+        public Guid SectorId { get; set; }
+        public string Name { get; set; }
+        public int TotalSeats { get; set; }
+        public int SoldSeats { get; set; }
+        public int AvailableSeats { get; set; }
+        public double OccupancyRate { get; set; }
+        public decimal Renueve { get; set; }
+    }
+}

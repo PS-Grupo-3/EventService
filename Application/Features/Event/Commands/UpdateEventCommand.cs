aa7dd2d54e68636187326a2ab700140b8e128125
alvaroramirezbrusco <alvaroramirezbrusco@gmail.com>
2025-11-24 20:43:31 -0300

    Endpoint de actualizar evento modificado

--- Application/Features/Event/Commands/UpdateEventCommand.cs
+++ Application/Features/Event/Commands/UpdateEventCommand.cs
@@ -3,4 +3,4 @@ using Application.Models.Responses;
 using MediatR;
 
 namespace Application.Features.Event.Commands;
-public record UpdateEventCommand(UpdateEventRequest Request, string? UserId, string? UserRole) : IRequest<GenericResponse>;
+public record UpdateEventCommand(Guid EventId, UpdateEventRequest Request, string? UserId, string? UserRole) : IRequest<GenericResponse>;
--- Application/Models/Requests/UpdateEventRequest.cs
+++ Application/Models/Requests/UpdateEventRequest.cs
@@ -2,8 +2,6 @@
 
 public class UpdateEventRequest
 {
-    public Guid EventId { get; set; }
-    public int? StatusId { get; set; }
     public string? Name { get; set; }
     public string? Description { get; set; }
     public DateTime? Time { get; set; }
